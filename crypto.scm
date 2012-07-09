ceff7e41330f1aeae62c615a822cfa90d0613681
Erik Falor <ewfalor@gmail.com>
2012-07-09 16:34:38 -0600

    builds in-memory database, but alists become too slow fast

--- crypto.scm
+++ crypto.scm
@@ -1,4 +1,5 @@
 #!/var/lib/bin/csi -s
+(use posix)
 
 ; return a function that upon each invocation returns the next
 ; letter of the alphabet, #f when exhausted
@@ -37,5 +38,31 @@
 				  (let ((n (next-letter)))
 					(cons n (loop (cdr chars)
 								  (cons (cons h n) letters)))))))))))))
+
+;Read a dictionary file and map each word to its pattern
+;Build up an alist keyed on these patterns
+(define prepare-dictionary
+  (lambda (filename)
+	(let ((alist '())
+		  (size (file-size filename)))
+	  (with-input-from-file
+		filename
+		(lambda ()
+		  (let loop ((word (read-line)) (lines 1))
+			(and (= 0 (remainder lines 500))
+				 (printf "\r~a/~a"
+						 (file-position (current-input-port)) size)
+				 (flush-output))
+			(if (eof-object? word)
+			  alist
+			  (let ((pattern (word->pattern word)))
+				(cond
+				  ((assoc pattern alist)
+				   => (lambda (match)
+						(append! match (list word))
+						(loop (read-line) (add1 lines))))
+				  (else
+					(set! alist (append alist (list (list pattern word))))
+					(loop (read-line) (add1 lines))))))))))))
 
 ; vim:set ft=scheme:
